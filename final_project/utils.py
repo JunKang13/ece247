4e0590acf5fac7bbc3eefa47f0fe80225809a3ff
Jun Kang <junkang13@ucla.edu>
2024-02-29 17:30:47 -0800

    final_update

--- final_project/utils.py
+++ final_project/utils.py
@@ -43,28 +43,24 @@ def load_data(X_train, y_train, X_test, y_test, verbose=False):
     X_test /= np.std(X_train, axis=0)
 
     # 5 folds including validation
-    train_subset, val_subset = torch.utils.data.random_split(X_train, [int(0.8 * X_train.shape[0]), int(0.2 * X_train.shape[0])],
+    X_tensor_train = torch.tensor(X_train, dtype=torch.float32)
+    y_tensor_train = torch.tensor(y_train, dtype=torch.long)
+    train_set = TensorDataset(X_tensor_train, y_tensor_train)
+    train_subset, val_subset = torch.utils.data.random_split(train_set,
+                                                             [int(0.8 * X_train.shape[0]), int(0.2 * X_train.shape[0])],
                                                              generator=torch.Generator().manual_seed(1))
-    X_new_train = X_train[train_subset.indices]
-    y_new_train = y_train[train_subset.indices]
+    # X_new_train = train_subset.dataset.data[train_subset.indices]
+    # y_new_train = train_subset.dataset.targets[train_subset.indices]
+    #
+    # X_new_val = val_subset.dataset.data[val_subset.indices]
+    # y_new_val = val_subset.dataset.targets[val_subset.indices]
+    #
+    # if verbose:
+    #     print("X_new_train shape: {}".format(X_new_train.shape))
+    #     print("y_new_train shape: {}".format(y_new_train.shape))
+    #     print("X_new_val shape: {}".format(X_new_val.shape))
+    #     print("y_new_val shape: {}".format(y_new_val.shape))
 
-    X_new_val = X_train[val_subset.indices]
-    y_new_val = y_train[val_subset.indices]
-
-    if verbose:
-        print("X_new_train shape: {}".format(X_new_train.shape))
-        print("y_new_train shape: {}".format(y_new_train.shape))
-        print("X_new_val shape: {}".format(X_new_val.shape))
-        print("y_new_val shape: {}".format(y_new_val.shape))
-
-    X_tensor_train = torch.tensor(X_new_train, dtype=torch.float32)
-    y_tensor_train = torch.tensor(y_new_train, dtype=torch.long)
-    train_set = IterableDataset(X_tensor_train, y_tensor_train)
-    train_loader = DataLoader(train_set, batch_size=32, shuffle=True)
-
-    return train_loader
-
-
-X_train, y_train, X_test, y_test = init_data(subject=None, verbose=True)
-train_loader = load_data(X_train, y_train, X_test, y_test, verbose=True)
-print(train_loader)
+    train_loader = torch.utils.data.DataLoader(train_subset, shuffle=True, batch_size=32)
+    val_loader = torch.utils.data.DataLoader(val_subset, shuffle=False, batch_size=32)
+    return train_loader, val_loader
