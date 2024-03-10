05a849a97d813eff32f3f8a0155c04ef983a72ef
Jun Kang <junkang13@ucla.edu>
2024-03-09 21:02:10 -0800

    preprocess_data update, data leakage solved.

--- final_project/utils.py
+++ final_project/utils.py
@@ -1,5 +1,9 @@
 import numpy as np
-from keras.utils import to_categorical
+from tensorflow import keras
+from tensorflow.keras.utils import to_categorical
+from scipy.signal import cwt, ricker
+import os
+os.environ['Cuda_VISIBLE_DEVICES'] = "0"
 
 
 def init_data(subject=None, verbose=False):
@@ -35,19 +39,35 @@ def init_data(subject=None, verbose=False):
 
 
 def preprocess_data(X_train, y_train, X_test, y_test, verbose=False):
-    X_train = X_train[:, :, :500]
+    ind_valid = np.random.choice(X_train.shape[0], int(0.25 * X_train.shape[0]), replace=False)
+    ind_train = np.array(list(set(range(X_train.shape[0])).difference(set(ind_valid))))
+    (x_train, x_valid) = X_train[ind_train], X_train[ind_valid]
+    (y_train, y_valid) = y_train[ind_train], y_train[ind_valid]
+
+    X_train = x_train[:, :, :500]
+    X_valid = x_valid[:, :, :500]
     X_test = X_test[:, :, :500]
     X__train_max = np.max(X_train.reshape(X_train.shape[0], X_train.shape[1], -1, 2), axis=3)
     X_test_max = np.max(X_test.reshape(X_test.shape[0], X_test.shape[1], -1, 2), axis=3)
     X_average_train = np.mean(X_train.reshape(X_train.shape[0], X_train.shape[1], -1, 2), axis=3)
     X_average_train = X_average_train + np.random.normal(0.0, 0.5, X_average_train.shape)
+    X__valid_max = np.max(X_valid.reshape(X_valid.shape[0], X_valid.shape[1], -1, 2), axis=3)
+    X_average_valid = np.mean(X_valid.reshape(X_valid.shape[0], X_valid.shape[1], -1, 2), axis=3)
+    X_average_valid = X_average_valid + np.random.normal(0.0, 0.5, X_average_valid.shape)
     total_X_train = np.vstack((X__train_max, X_average_train))
     total_y_train = np.hstack((y_train, y_train))
+    total_X_valid = np.vstack((X__valid_max, X_average_valid))
+    total_y_valid = np.hstack((y_valid, y_valid))
     for i in range(2):
         X_subsample = X_train[:, :, i::2] + np.random.normal(0.0, 0.5, X_train[:, :, i::2].shape)
 
         total_X_train = np.vstack((total_X_train, X_subsample))
         total_y_train = np.hstack((total_y_train, y_train))
+
+    for i in range(2):
+        X_subsample = X_valid[:, :, i::2] + np.random.normal(0.0, 0.5, X_valid[:, :, i::2].shape)
+        total_X_valid = np.vstack((total_X_valid, X_subsample))
+        total_y_valid = np.hstack((total_y_valid, y_valid))
 
     if verbose:
         print("Training data shape: {}".format(total_X_train.shape), end=" ")
@@ -55,10 +75,10 @@ def preprocess_data(X_train, y_train, X_test, y_test, verbose=False):
         print("Testing data shape: {}".format(X_test_max.shape), end=" ")
         print("Testing target shape: {}".format(y_test.shape))
 
-    return total_X_train, total_y_train, X_test_max, y_test
+    return total_X_train, total_y_train, X_test_max, y_test, total_X_valid, total_y_valid
 
 
-def window_data(X_train, y_train, window_size=200, stride=100, verbose=False):
+def window_data(X_train, y_train, window_size=250, stride=100, verbose=False):
     num_samples, num_features, num_timepoints = X_train.shape
     num_windows = (num_timepoints - window_size) // stride
     X_train_window = np.zeros((num_samples * num_windows, num_features, window_size))
@@ -71,19 +91,28 @@ def window_data(X_train, y_train, window_size=200, stride=100, verbose=False):
                 y_train_window[i * num_windows + k] = y_train[i]
 
     if verbose:
-        print("WTraining data shape: {}".format(X_train_window.shape), end=" ")
-        print("WTraining target shape: {}".format(y_train_window.shape))
+        print("Windowed Training data shape: {}".format(X_train_window.shape), end=" ")
+        print("Windowed Training target shape: {}".format(y_train_window.shape))
 
     return X_train_window, y_train_window
 
 
+# CWT
+def cwt_transform(eeg, scale, wavelet, verbose=False):
+    num_samples, num_features, num_timepoints = eeg.shape
+    eeg_cwt = np.zeros((num_samples, num_features, len(scale), num_timepoints))
 
-def load_data(X_train, y_train, X_test, y_test, verbose=False):
+    for i in range(num_samples):
+        for j in range(num_features):
+            cwt_temp = cwt(eeg[i, j, :], wavelet, scale)
+            eeg_cwt[i, j, :, :] = cwt_temp
 
-    ind_valid = np.random.choice(X_train.shape[0], int(0.2 * X_train.shape[0]), replace=False)
-    ind_train = np.array(list(set(range(X_train.shape[0])).difference(set(ind_valid))))
-    (x_train, x_valid) = X_train[ind_train], X_train[ind_valid]
-    (y_train, y_valid) = y_train[ind_train], y_train[ind_valid]
+    if verbose:
+        print("CWT Transformed data shape: {}".format(eeg_cwt.shape))
+    return eeg_cwt
+
+
+def load_data(x_train, y_train, x_valid, y_valid, X_test, y_test, verbose=False):
 
     y_train = to_categorical(y_train, 4)
     y_valid = to_categorical(y_valid, 4)
@@ -109,19 +138,4 @@ def load_data(X_train, y_train, X_test, y_test, verbose=False):
         print("Testing data shape: {}".format(x_test.shape), end=" ")
         print("Testing target shape: {}".format(y_test.shape))
 
-    # 5 folds including validation
-    # X_tensor_train = torch.tensor(X_train, dtype=torch.float32)
-    # y_tensor_train = torch.tensor(y_train, dtype=torch.long)
-    # X_tensor_test = torch.tensor(X_test, dtype=torch.float32)
-    # y_tensor_test = torch.tensor(y_test, dtype=torch.long)
-
-    # train_set = TensorDataset(X_tensor_train, y_tensor_train)
-    # train_subset, val_subset = torch.utils.data.random_split(train_set,
-    #                                                          [int(0.8 * X_train.shape[0]), int(0.2 * X_train.shape[0])],
-    #                                                          generator=torch.Generator().manual_seed(1))
-    #
-    # train_loader = torch.utils.data.DataLoader(train_subset, shuffle=True, batch_size=32)
-    # val_loader = torch.utils.data.DataLoader(val_subset, shuffle=False, batch_size=32)
-    # test_loader = torch.utils.data.DataLoader(TensorDataset(X_tensor_test, y_tensor_test))
-    # return train_loader, val_loader, test_loader
     return x_train, y_train, x_valid, y_valid, x_test, y_test
