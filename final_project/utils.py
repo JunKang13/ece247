7a0b0cdd3edf7193a0d718bdd47bdd2b843abd6c
CreeperZzz <Chris_hsw@outlook.com>
2024-03-11 17:10:35 -0700

    adjust model

--- final_project/utils.py
+++ final_project/utils.py
@@ -39,14 +39,14 @@ def init_data(subject=None, verbose=False):
 
 
 def preprocess_data(X_train, y_train, X_test, y_test, verbose=False):
-    ind_valid = np.random.choice(X_train.shape[0], int(0.25 * X_train.shape[0]), replace=False)
+    ind_valid = np.random.choice(X_train.shape[0], int(0.1 * X_train.shape[0]), replace=False)
     ind_train = np.array(list(set(range(X_train.shape[0])).difference(set(ind_valid))))
     (x_train, x_valid) = X_train[ind_train], X_train[ind_valid]
     (y_train, y_valid) = y_train[ind_train], y_train[ind_valid]
 
-    X_train = x_train[:, :, :500]
-    X_valid = x_valid[:, :, :500]
-    X_test = X_test[:, :, :500]
+    X_train = x_train[:, :, :800]
+    X_valid = x_valid[:, :, :800]
+    X_test = X_test[:, :, :800]
     X__train_max = np.max(X_train.reshape(X_train.shape[0], X_train.shape[1], -1, 2), axis=3)
     X_test_max = np.max(X_test.reshape(X_test.shape[0], X_test.shape[1], -1, 2), axis=3)
     X_average_train = np.mean(X_train.reshape(X_train.shape[0], X_train.shape[1], -1, 2), axis=3)
